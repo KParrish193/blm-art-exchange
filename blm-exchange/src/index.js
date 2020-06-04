82262d8dc9743ab2529abb327bb60e24fb37854f
KP Parrish <57117452+KParrish193@users.noreply.github.com>
2020-06-03 20:02:33 -0600

    added contexts

--- blm-exchange/src/index.js
+++ blm-exchange/src/index.js
@@ -3,14 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+import { ArtistContext } from './contexts/ArtistContext';
+import { CartContext } from './contexts/CartContext';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.render(
+  
+  <ArtistContext.Provider>
+  <CartContext.Provider>
   <Router>
   <React.StrictMode>
     <App />
   </React.StrictMode>
-  </Router>,
+  </Router>
+  </CartContext.Provider>
+  </ArtistContext.Provider>,
   document.getElementById('root')
 );
 

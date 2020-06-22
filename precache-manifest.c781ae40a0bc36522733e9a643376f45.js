fd115a0883cc591142e757c98526d5a4898aa3fb
KP Parrish <57117452+KParrish193@users.noreply.github.com>
2020-06-22 11:52:39 -0600

    Updates

---
+++ precache-manifest.c781ae40a0bc36522733e9a643376f45.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c49ed332a326c28783c1cf57e0a08bed",
+    "url": "/blm-art-exchange/index.html"
+  },
+  {
+    "revision": "5dfb8b25193eacbea7fc",
+    "url": "/blm-art-exchange/static/css/main.8f93d9eb.chunk.css"
+  },
+  {
+    "revision": "2365f3aee3ddb83bda63",
+    "url": "/blm-art-exchange/static/js/2.68928c6a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/blm-art-exchange/static/js/2.68928c6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5dfb8b25193eacbea7fc",
+    "url": "/blm-art-exchange/static/js/main.b9336c9a.chunk.js"
+  },
+  {
+    "revision": "c09347a0f08800371102",
+    "url": "/blm-art-exchange/static/js/runtime-main.a8d64fcb.js"
+  },
+  {
+    "revision": "a4a79c5bb4551b30c9a55dad696646b0",
+    "url": "/blm-art-exchange/static/media/lightsimplelogo.a4a79c5b.png"
+  }
+]);

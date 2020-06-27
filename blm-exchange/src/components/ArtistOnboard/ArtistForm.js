cebd0d925a7c89176a2db5e54dab715474c21c54
KP Parrish <57117452+KParrish193@users.noreply.github.com>
2020-06-26 23:25:37 -0600

    updated header to include active styling

--- blm-exchange/src/components/ArtistOnboard/ArtistForm.js
+++ blm-exchange/src/components/ArtistOnboard/ArtistForm.js
@@ -1,12 +1,25 @@
 import React from 'react'
 
+import CoC from './CodeOfConduct';
+
+import { PrimaryButton } from '../../global styles/index';
+
 import { } from 'react-hook-form';
 
 function ArtistForm(){
+    function onSubmit(){
+
+    } 
 
     return(
         <div>
             {/* artist sign-up form */}
+            
+            
+            {/* modal to confirm code of conduct for new artists */}
+            
+            <CoC />
+            <PrimaryButton onclick={onSubmit} > Submit </PrimaryButton>
         </div>
     )
 }
--- blm-exchange/src/components/Inquiry.js
+++ blm-exchange/src/components/Inquiry.js
@@ -1,13 +1,20 @@
 import React from 'react'
 
+import { PrimaryButton } from '../global styles/index'
 import { } from 'react-hook-form';
 
+
 function Inquiry(){
+
+    function onSubmit(){
+
+    }
 
     return(
         <div>
             {/* form to contact artist for specific requests */}
             {/* dynamically populate artist contact info from artist context/filter based on artist opt-in */}
+        <PrimaryButton onclick={onSubmit} > Submit </PrimaryButton>
         </div>
     )
 }

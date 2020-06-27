ba26a9970287339ec7f2aebf830e2155d6e03bdd
Shilpa Kancharla <shilpakancharla18@gmail.com>
2020-06-27 18:16:55 -0400

    Secured adobe fonts

--- blm-exchange/src/components/About.js
+++ blm-exchange/src/components/About.js
@@ -8,15 +8,19 @@ function About(){
             <h1>Art x BLM</h1>
             <p>This is where the tagline goes.</p>
             <h2><b>Mission</b></h2>
-            <p>This is where the mission goes. This is where the mission goes. This is where the mission goes.
-            This is where the mission goes. This is where the mission goes. This is where the mission goes.
-            This is where the mission goes. This is where the mission goes. This is where the mission goes.
-            </p>
-            <p><b>Donations to date</b></p>
-            {/* Progress bar */}
-            <ProgressBar variant="success" now={60}></ProgressBar>
-            <Button>Shop</Button>
-            <Button>Artists</Button>
+            <div class="container">
+                <p>This is where the mission goes. This is where the mission goes. This is where the mission goes.
+                This is where the mission goes. This is where the mission goes. This is where the mission goes.
+                This is where the mission goes. This is where the mission goes. This is where the mission goes.
+                </p>
+            </div>
+            <div class="container">
+                <p><b>Donations to date</b></p>
+                {/* Progress bar */}
+                <ProgressBar variant="success" now={60}></ProgressBar>
+                <Button>Shop</Button>
+                <Button>Artists</Button>
+            </div>
         </div>
     );
 }

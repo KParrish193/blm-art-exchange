8b8782ad6c16bde7d74eaf1f34bf66a1084a0227
Shilpa Kancharla <shilpakancharla18@gmail.com>
2020-06-28 14:18:09 -0400

    Added preliminary layout of artist list

--- blm-exchange/src/components/ArtistList.js
+++ blm-exchange/src/components/ArtistList.js
@@ -3,32 +3,50 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Image from 'react-bootstrap/Image';
+import wave1 from '../assets/art_samples/cerulean_blue.JPG';
+import wave2 from '../assets/art_samples/indigo_dark.JPG'
 
 function ArtistList() {
 
     return(
-        <div class="container">
+        <Container>
             <Row>
-                <Col>
-                    <Image src="../../assets/art_samples/cerulean_blue.JPG" fluid />
-                    First Last
+                <Col xs={3} md={3}>
+                    <Image src={wave1} width={180} height={180} rounded fluid thumbnail/>
                 </Col>
-                <Col>
-                    <Image src="../assets/art_samples/indigo_dark.JPG" fluid />
-                    First Last
+                <Col xs={3} md={3}>
+                    <h3>First Last</h3>
+                    <p>Bio & details go here. Bio & details go here. Bio & details go here.</p>
+                    <p>Link</p>
+                </Col>
+                <Col xs={3} md={3}>
+                    <Image src={wave2} width={180} height={180} rounded fluid thumbnail/>
+                </Col>
+                <Col xs={3} md={3}>
+                    <h3>First Last</h3>
+                    <p>Bio & details go here. Bio & details go here. Bio & details go here.</p>
+                    <p>Link</p>
                 </Col>
             </Row>
             <Row>
-                <Col>
-                    <Image src="../assets/art_samples/indigo_light.JPG" fluid />
-                    First Last
+                <Col xs={3} md={3}>
+                    <Image src={wave1} width={180} height={180} rounded fluid thumbnail/>
                 </Col>
-                <Col>
-                    <Image src="../assets/art_samples/malachite.JPG" fluid />
-                    First Last
+                <Col xs={3} md={3}>
+                    <h3>First Last</h3>
+                    <p>Bio & details go here. Bio & details go here. Bio & details go here.</p>
+                    <p>Link</p>
+                </Col>
+                <Col xs={3} md={3}>
+                    <Image src={wave2} width={180} height={180} rounded fluid thumbnail/>
+                </Col>
+                <Col xs={3} md={3}>
+                    <h3>First Last</h3>
+                    <p>Bio & details go here. Bio & details go here. Bio & details go here.</p>
+                    <p>Link</p>
                 </Col>
             </Row>
-        </div>
+        </Container>
     )
 }
 
